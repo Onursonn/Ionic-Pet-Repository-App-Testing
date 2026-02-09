f743d6f7a2738920e257c6f3f5c597f5e652cd06
Onursonn <97085413+Onursonn@users.noreply.github.com>
2026-02-09 09:18:25 +0100

    clean up App from unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
-import { Redirect, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import {
   IonApp,
-  IonIcon,
-  IonLabel,
   IonRouterOutlet,
-  IonTabBar,
-  IonTabButton,
-  IonTabs,
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -51,9 +42,9 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-          <Route path="/">
-            <Home />
-          </Route>
+        <Route path="/">
+          <Home />
+        </Route>
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
